72934fa03563a7fcd937a286bc7d69c58825f802
AFK-NOCAP <gagan@odrways.com>
2024-02-29 15:44:04 +0530

    first commit

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 angular.module('myApp').constant('API_CONSTANTS', {
-    BASE_URL: 'http://localhost:9072',
+    BASE_URL: 'http://35.207.210.0',
     LOGIN_ENDPOINT: '/api/v1/auth/login',
     GET_ALL_APPLICATIONS: '/api/v1/application'
 });
